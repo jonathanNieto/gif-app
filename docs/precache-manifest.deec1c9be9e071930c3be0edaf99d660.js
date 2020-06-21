3af370d82fa59bac00ddb247edf2d3d66771cd2b
Jonathan Nieto <40543257+jonathanNieto@users.noreply.github.com>
2020-06-20 21:48:11 -0600

    docs folder added

---
+++ docs/precache-manifest.deec1c9be9e071930c3be0edaf99d660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed67226798d9b060e09e230db345629e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df6c18e2f8eeea475cdc",
+    "url": "/static/css/main.1ea18284.chunk.css"
+  },
+  {
+    "revision": "fc8297614e4f448f12ef",
+    "url": "/static/js/2.969b929d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.969b929d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df6c18e2f8eeea475cdc",
+    "url": "/static/js/main.f1640131.chunk.js"
+  },
+  {
+    "revision": "ded719702d9692d2c54c",
+    "url": "/static/js/runtime-main.6ecacd89.js"
+  }
+]);
